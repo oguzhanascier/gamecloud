98a5649660440d36fa6c6e17d6d8c969934df759
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-23 13:44:25 +0300

    upgrade firebase config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,31 @@
-import firebase from 'firebase/app'
+import firebase from "firebase/compat/app";
+
+import "firebase/compat/firestore";
+
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDFVqNoH3zchuEHkAbdqdVpgQLuheBE-CQ",
+
+    authDomain: "blog-app-7cc8a.firebaseapp.com",
+
+    databaseURL: "https://blog-app-7cc8a-default-rtdb.firebaseio.com",
+
+    projectId: "blog-app-7cc8a",
+
+    storageBucket: "blog-app-7cc8a.appspot.com",
+
+    messagingSenderId: "291121184274",
+
+    appId: "1:291121184274:web:440e17c5e5f7581fd122cb",
+
+    measurementId: "G-NLDKV13V8W"
+
+};
+
+firebase.initializeApp(firebaseConfig)
+const firestoreRef = firebase.firestore()
+const authRef = firebase.auth()
+
+export {firestoreRef, authRef}
