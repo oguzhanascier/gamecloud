bc147a28b0cbd4bf09f0f89499184c90be25dea0
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-23 14:01:05 +0300

    organizing

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,9 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      return import( '../views/AboutView.vue')
     }
   }
 ]
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,17 @@
 <template>
-  
+  <div class="about">
+    <h1>Hakkımızda</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis nesciunt laborum praesentium sed omnis provident eaque aspernatur repudiandae obcaecati fuga fugit, autem dignissimos, labore, veritatis accusantium consectetur. Provident, odio iste.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis nesciunt laborum praesentium sed omnis provident eaque aspernatur repudiandae obcaecati fuga fugit, autem dignissimos, labore, veritatis accusantium consectetur. Provident, odio iste.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis nesciunt laborum praesentium sed omnis provident eaque aspernatur repudiandae obcaecati fuga fugit, autem dignissimos, labore, veritatis accusantium consectetur. Provident, odio iste.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis nesciunt laborum praesentium sed omnis provident eaque aspernatur repudiandae obcaecati fuga fugit, autem dignissimos, labore, veritatis accusantium consectetur. Provident, odio iste.</p>
+  </div>
 </template>
+
+
+<style scoped>
+ .about{
+  border: 2px solid rebeccapurple;
+  padding: 30px;
+ }
+</style>
