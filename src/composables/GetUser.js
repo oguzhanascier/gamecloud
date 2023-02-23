045d39a4bff988d4f937d2081e3fb5b098aebbd6
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-23 15:55:22 +0300

    created getUser composable

---
+++ src/composables/GetUser.js
@@ -0,0 +1,14 @@
+import { ref } from 'vue'
+import { authRef } from '@/firebase/config'
+
+const kullanici = ref(authRef.currentUser)
+
+authRef.onAuthStateChanged(k => {
+    kullanici.value = k
+})
+
+const getUser = () => {
+    return { kullanici }
+}
+
+export default getUser
