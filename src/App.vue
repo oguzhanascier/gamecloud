fe1e88dbcd0b3ff2e4bcc2caf80b81b68ead2d6a
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-24 20:03:59 +0300

    logout

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
-    <div v-if="!user" class="nav">
-        <router-link :to="{ name: 'ekle' }" class="router">Oyun Ekle</router-link>
-        <router-link :to="{ name: 'oyunlar' }" class="router">Oyunlar</router-link>
-    </div>
-    <div class="nav" v-else>
-        <router-link :to="{ name: 'home' }" class="router">Anasayfa</router-link>
-        <router-link :to="{ name: 'about' }" class="router">Hakkımızda</router-link>
+    <div>
+        <div v-if="kullanici" class="nav">
+            <router-link :to="{ name: 'ekle' }" class="router">Oyun Ekle</router-link>
+            <router-link :to="{ name: 'oyunlar' }" class="router">Oyunlar</router-link>
+            <button @click="handleCikis" class="out">Çıkış Yap</button>
+        </div>
+        <div class="nav" v-else>
+            <router-link :to="{ name: 'home' }" class="router">Anasayfa</router-link>
+            <router-link :to="{ name: 'about' }" class="router">Hakkımızda</router-link>
+            
+        </div>
     </div>
     <router-view></router-view>
 </template>
 
 <script>
 import getUser from './composables/GetUser'
+import cikisYap from './composables/CikisYap'
+import { useRouter } from 'vue-router'
 export default {
     setup() {
-        const { user } = getUser()
+        const { kullanici } = getUser()
+        const { logout } = cikisYap()
+        const router = useRouter()
+        console.log(kullanici);
+        const handleCikis = async () => {
+            await logout();
+            router.push('/')
 
-        return { user }
+        }
+
+        return { kullanici,  handleCikis }
     }
 }
 </script>
@@ -37,5 +51,12 @@ body {
     text-decoration: none;
     margin: 20px;
     border-bottom: 1px solid white;
+}
+
+.out{
+background: rgb(255, 255, 255);
+border: none;
+border-radius: 50%;
+cursor: pointer;
 }
 </style>
