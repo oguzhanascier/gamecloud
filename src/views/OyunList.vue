c5ccaf096534a6d60e0241208f28b4b157a18aa4
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-26 13:10:57 +0300

    oyunList binding and style operations

--- src/views/OyunList.vue
+++ src/views/OyunList.vue
@@ -1,13 +1,50 @@
 <template>
-    <div>
-
+    <div class="oyunList">
+        <h1>Oyun Listesi</h1>
+        <ul>
+            <li v-for="oyun in oyunlar" :key="oyun.id">
+                <span>videogame-assest</span>
+                <div>
+                    {{ oyun.oyunAd }}
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
-<script setup lang="ts">
-
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+export default {
+    setup() {
+        const store = useStore()
+        const oyunlar = computed(() => {
+            return store.state.oyunlar;
+        })
+        return { oyunlar }
+    }
+}
 </script>
 
 <style scoped>
+.oyınList ul {
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 20px;
+    max-width: 600px;
+    margin: 60px auto;
+}
+
+.oyunList li {
+    list-style-type: none;
+    background: white;
+    padding: 30px;
+    border-radius: 10px;
+    box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    line-height: 1.5em;
+    text-transform: capitalize;
+}
 
 </style>
