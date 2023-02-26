dc2de8350a2c8ccdddc945c6f7ca63034e51753e
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-26 13:17:31 +0300

    document processing and deletion

--- src/composables/docProcess.js
+++ src/composables/docProcess.js
@@ -1,2 +1,31 @@
 import { ref } from 'vue'
 import { firestoreRef } from '@/firebase/config'
+
+
+const docProcess = (collection) => {
+    const hata = ref(null)
+
+    const ekle = async (doc) => {
+        hata.value = null
+
+        try {
+            const res = await firestoreRef.collection(collection).add(doc)
+            return res
+        } catch (error) {
+            hata.value = 'Belge Eklenemedi'
+        }
+    }
+    const sil = async (id) => {
+        hata.value = null
+
+        try {
+            const res = await firestoreRef.collection(collection).doc(id).delete()
+            return res
+        } catch (error) {
+            hata.value = 'Belge Silinemedi'
+        }
+    }
+    return { hata, ekle, sil }
+}
+
+export default docProcess
