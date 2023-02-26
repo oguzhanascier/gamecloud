68ac0c905424a36c8a5b1cec545a954b7bf253da
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-26 13:22:48 +0300

    oyun deletion and processing

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,36 @@
 import { createStore } from 'vuex'
 import getDocuments from '@/composables/getDocument'
+import docProcess from '@/composables/docProcess'
 
-const {err, documents}= getDocuments('oyunlar')
+const { error, documents } = getDocuments('oyunlar')
+const { hata, ekle, sil } = docProcess('oyunlar')
 
 export default createStore({
   state: {
-    oyunlar:documents,
-    err:err
+    oyunlar: documents,
+    error: error,
+    hata:hata
   },
   getters: {
   },
   mutations: {
+    ekleMutation(state, oyun) {
+      let oyunN = {
+        oyundAd: oyun
+      }
+      ekle(oyunN)
+    },
+    silMutation(state, id) {
+      sil(id)
+    }
   },
   actions: {
+    oyunEkleAction(context, oyun) {
+      context.commit('EkleMutation', oyun)
+    },
+    oyunSilAction(context, id) {
+      context.commit('silMutation', id)
+    }
   },
   modules: {
   }
