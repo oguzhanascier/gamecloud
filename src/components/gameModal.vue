49424d2af9f2ad4450650593294698e0839819cf
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-27 14:30:48 +0300

    created modal

---
+++ src/components/gameModal.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="modal-wrapper">
+        <div class="modal">
+            Lütfen Oyun Adını giriniz
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.modal-wrapper{
+    position: fixed;
+    width: 100%;
+    top: 20px;
+}
+
+.modal{
+    padding: 20px;
+    border: 1px solid white;
+    background: rebeccapurple;
+    border-radius: 10px;
+    box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.2);
+    max-width: 400px;
+    margin: 0 auto;
+}
+</style>
