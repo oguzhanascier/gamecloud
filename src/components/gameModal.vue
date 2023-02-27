8c153cbd4e52d5c96707ba6eb7610441b4054721
oguzhanascier <oguzhanascier20@gmail.com>
2023-02-27 14:41:39 +0300

    style

--- src/components/gameModal.vue
+++ src/components/gameModal.vue
@@ -12,17 +12,18 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style>
 .modal-wrapper{
     position: fixed;
     width: 100%;
-    top: 20px;
+    top: 20%;
 }
 
 .modal{
     padding: 20px;
     border: 1px solid white;
-    background: rebeccapurple;
+    color: white;
+    background: rgb(126, 82, 170);
     border-radius: 10px;
     box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.2);
     max-width: 400px;
